da77a2c4b33a2a2b224b291f7dd0c51324740280
Ruan Anjos <anjosruanp@gmail.com>
2025-11-07 22:43:42 -0300

    refactor: remove unused API configuration and consolidate API_KEY in app.js

--- src/scripts/api.js
+++
@@ -1,4 +0,0 @@
-const API_URL = "https://themoviedb.org/";
-const API_KEY = "061ef10fb0dbfe0515d15b2e76255380";
-
-export { API_URL, API_KEY };
---
+++ src/scripts/app.js
@@ -0,0 +1,5 @@
+const API_KEY = '061ef10fb0dbfe0515d15b2e76255380';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p';
+
+export { API_URL, API_KEY };
